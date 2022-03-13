41ad7ba52212cbedc0ade996496545613091c53e
Disneyhockey40 <85129910+Disneyhockey40@users.noreply.github.com>
2022-03-12 22:25:20 -0500

    Update Uninstaller.py

--- Uninstaller.py
+++ Uninstaller.py
@@ -1,7 +1,7 @@
 ## Uninstaller 1.0.0 - Oszust Industries
 from os import path
 import os, shutil, win32com.client
-exit() ## STOPS THE UNINSTALLER
+##exit() ## STOPS THE UNINSTALLER
 objShell, uninstallerPath = win32com.client.Dispatch("WScript.Shell"), os.path.dirname(os.path.realpath(__file__))
 try: os.remove(objShell.SpecialFolders("AllUsersPrograms") + "\\Oszust Industries\\" + uninstallerPath.split("\\")[-1]) ## Delete Shortcut
 except: pass
