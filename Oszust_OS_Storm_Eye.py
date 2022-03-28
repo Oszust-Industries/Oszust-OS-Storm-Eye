3f0b13d0452d3af77b46b56a2378603a94259de5
Disneyhockey40 <85129910+Disneyhockey40@users.noreply.github.com>
2022-03-28 00:50:43 -0400

    Update Oszust_OS_Storm_Eye.py

--- Oszust_OS_Storm_Eye.py
+++ Oszust_OS_Storm_Eye.py
@@ -1,6 +1,6 @@
 ## Oszust OS Storm Eye - Oszust Industries
-## Created on: 12-16-21 - Last update: 3-27-21
-softwareVersion = "ALPHA-v1.0.1.013"
+## Created on: 12-16-21 - Last update: 3-28-21
+softwareVersion = "ALPHA-v1.0.1.018"
 def clear(): return ("\n" * 70)
 from urllib.request import urlopen
 from pathlib import Path
@@ -102,16 +102,13 @@ def checkUpdateStatus():
             exitText = input(clear() + "An emergency has been downloaded.\nThe update has fixed critical issues.\n\nPress any key to exit "+systemName+" and finish the installation.")
             exit()
         elif AutoUpdater.UpdateStatus in [1, 2]:
-            toaster.show_toast(systemName + ": New Update Installed", "Relaunch the app to finish the update.", icon_path = str(Path(__file__).resolve().parent) + "\\DownloadIcon.ico", duration = 8, threaded = True, callback_on_click=notificationClickActions("restart"))
+            toaster.show_toast(systemName + ": New Update Installed", "Relaunch the app to finish the installation.", icon_path = str(Path(__file__).resolve().parent) + "\\DownloadIcon.ico", duration = 8, threaded = True)
             return "Update Cleared"
         elif AutoUpdater.UpdateStatus == -3:
             toaster.show_toast(systemName + ": AutoUpdater Failed", "A requested app build does not exist.", icon_path = str(Path(__file__).resolve().parent) + "\\DownloadIcon.ico", duration = 8, threaded = True)
             return "Update Failed"
         elif AutoUpdater.UpdateStatus in [-2, 0]: return "Update Cleared"
         else: time.sleep(0.3)
-
-def notificationClickActions(Action):
-    if Action == "restart": exit()
 
 def crashMessage():
     ## Display Crash
