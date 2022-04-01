fef0cffc5e302330c7ab6ac1b4bba4ba055e11bb
Disneyhockey40 <85129910+Disneyhockey40@users.noreply.github.com>
2022-04-01 12:38:10 -0400

    Update Uninstaller.py

--- Uninstaller.py
+++ Uninstaller.py
@@ -1,8 +1,9 @@
-## Uninstaller 1.0.0 - Oszust Industries
-from os import path
+## Uninstaller 1.1.0 - Oszust Industries
+from win32com.shell import shell, shellcon
 import os, shutil, win32com.client
-##exit() ## STOPS THE UNINSTALLER
 objShell, uninstallerPath = win32com.client.Dispatch("WScript.Shell"), os.path.dirname(os.path.realpath(__file__))
-try: os.remove(objShell.SpecialFolders("AllUsersPrograms") + "\\Oszust Industries\\" + uninstallerPath.split("\\")[-1]) ## Delete Shortcut
+try: os.remove(objShell.SpecialFolders("AllUsersPrograms") + "\\Oszust Industries\\" + uninstallerPath.split("\\")[-1] + ".lnk") ## Delete Start Menu Shortcut
+except: pass
+try: os.remove(shell.SHGetFolderPath (0, shellcon.CSIDL_DESKTOP, 0, 0) + "\\" + uninstallerPath.split("\\")[-1] + ".lnk") ## Delete Desktop Shortcut
 except: pass
 shutil.rmtree(uninstallerPath) ## Delete Program
