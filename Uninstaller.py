21147c4a1d747f7a7eb8088fad5ffaa3b37305d6
Disneyhockey40 <85129910+Disneyhockey40@users.noreply.github.com>
2022-04-01 17:54:49 -0400

    Update Uninstaller.py

--- Uninstaller.py
+++ Uninstaller.py
@@ -1,6 +1,7 @@
 ## Uninstaller 1.1.0 - Oszust Industries
 from win32com.shell import shell, shellcon
 import os, shutil, win32com.client
+print("Deleting Program...")
 objShell, uninstallerPath = win32com.client.Dispatch("WScript.Shell"), os.path.dirname(os.path.realpath(__file__))
 try: os.remove(objShell.SpecialFolders("AllUsersPrograms") + "\\Oszust Industries\\" + uninstallerPath.split("\\")[-1] + ".lnk") ## Delete Start Menu Shortcut
 except: pass
